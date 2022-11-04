cee430a005096d18d29062de057ed9fc8b06b9a3
Franz-SuperCode <franzsupercode@gmail.com>
2022-11-04 15:43:03 +0100

    im html

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,28 +3,32 @@ const steinButton = document.querySelector("#stein");
 const papierButton = document.querySelector("#papier");
 const schereButton = document.querySelector("#schere");
 // Output geholt
-const outputUserWin = document.querySelector("#userWin");
-const outputCompWin = document.querySelector("#compWin");
+let outputUserWin = document.querySelector("#userWin");
+let outputCompWin = document.querySelector("#compWin");
 
-//COMP Zufall zwischen 0-2
-const compAuswahl = Math.floor(Math.random() * 3);
-console.log(compAuswahl);
-
-// Hat der COMP nun Stein, Papier oder Schere?
-if (compAuswahl === 0) {
-    console.log("Compauswahl ist Stein")
-} else if (compAuswahl === 1) {
-    console.log("Compauswahl ist Papier")
-} else {
-    console.log("Compauswahl ist Schere")
-}
-
-
+//Variablen für User/COMP Auswahl (Stein, Papier oder Schere)
+let compAuswahl;
 let userAuswahl;
-//Beim click soll der Wert in die Variable gepeichert werden
+
+
 steinButton.addEventListener("click", () => {
+    //Beim click soll der Wert in die Variable gepeichert werden
     userAuswahl = 0;
     console.log('Userauswahl ist Stein');
+
+    //COMP Zufall zwischen 0-2
+    compAuswahl = Math.floor(Math.random() * 3);
+    console.log(compAuswahl);
+
+    // Hat der COMP nun Stein, Papier oder Schere?
+    if (compAuswahl === 0) {
+        console.log("Compauswahl ist Stein")
+    } else if (compAuswahl === 1) {
+        console.log("Compauswahl ist Papier")
+    } else {
+        console.log("Compauswahl ist Schere")
+    }
+
     whoWin();
     roundsToWin();
 })
@@ -32,6 +36,19 @@ steinButton.addEventListener("click", () => {
 papierButton.addEventListener("click", () => {
     userAuswahl = 1;
     console.log('Userauswahl ist Papier');
+
+    compAuswahl = Math.floor(Math.random() * 3);
+    console.log(compAuswahl);
+
+    // Hat der COMP nun Stein, Papier oder Schere?
+    if (compAuswahl === 0) {
+        console.log("Compauswahl ist Stein")
+    } else if (compAuswahl === 1) {
+        console.log("Compauswahl ist Papier")
+    } else {
+        console.log("Compauswahl ist Schere")
+    }
+
     whoWin();
     roundsToWin();
 })
@@ -39,6 +56,19 @@ papierButton.addEventListener("click", () => {
 schereButton.addEventListener("click", () => {
     userAuswahl = 2;
     console.log('Userauswahl ist Schere');
+
+    compAuswahl = Math.floor(Math.random() * 3);
+    console.log(compAuswahl);
+
+    // Hat der COMP nun Stein, Papier oder Schere?
+    if (compAuswahl === 0) {
+        console.log("Compauswahl ist Stein")
+    } else if (compAuswahl === 1) {
+        console.log("Compauswahl ist Papier")
+    } else {
+        console.log("Compauswahl ist Schere")
+    }
+
     whoWin();
     roundsToWin();
 })
@@ -53,86 +83,63 @@ const whoWin = () => {
         console.log("COMP Loose");
         //Gewinn auf den Zählerstand packen
         winCountUser += 1;
-        console.log(`Wincount User: ${winCountUser}`);
-        console.log(`wincount COMP: ${wincountComp}`);
-        return;
+
 
     } else if (compAuswahl === 0 && userAuswahl === 2) {
         console.log("COMP Win");
         wincountComp += 1;
-        console.log(`Wincount User: ${winCountUser}`);
-        console.log(`wincount COMP: ${wincountComp}`);
-        return
 
     } else if (compAuswahl === 0 && userAuswahl === 0) {
         console.log("Unentschieden");
-        return;
+
     }
 
 
     if (compAuswahl === 1 && userAuswahl === 1) {
         console.log("Unentschieden");
-        return;
+
 
     } else if (compAuswahl === 1 && userAuswahl === 2) {
         console.log("COMP Loose");
         winCountUser += 1;
-        console.log(`Wincount User: ${winCountUser}`);
-        console.log(`wincount COMP: ${wincountComp}`);
-        return
+
 
     } else if (compAuswahl === 1 && userAuswahl === 0) {
         console.log("COMP Win");
         wincountComp += 1;
-        console.log(`Wincount User: ${winCountUser}`);
-        console.log(`wincount COMP: ${wincountComp}`);
-        return;
+
     }
 
     if (compAuswahl === 2 && userAuswahl === 1) {
         console.log("COMP Win");
         wincountComp += 1;
-        console.log(`Wincount User: ${winCountUser}`);
-        console.log(`wincount COMP: ${wincountComp}`);
-        return;
 
     } else if (compAuswahl === 2 && userAuswahl === 2) {
         console.log("Unentschieden");
-        console.log(`Wincount User: ${winCountUser}`);
-        console.log(`wincount COMP: ${wincountComp}`);
-        return
+
 
     } else if (compAuswahl === 2 && userAuswahl === 0) {
         console.log("COMP Loose");
         winCountUser += 1;
-        return;
     }
 
+    outputUserWin.innerHTML = winCountUser;
+    outputCompWin.innerHTML = wincountComp;
 }
-//Ausrechnen wer nun Final Gewonnen hat
-// const finalwin5 = () => {
 
-//     if (winCountUser === 5) {
-//         console.log("FINAL WINNER: USER");
-//         return;
-//     }
-
-//     if (wincountComp === 5) {
-//         console.log("FINAL WINNER: COMP");
-//         return;
-//     }
-// }
-
+// Bei reset
 document.querySelector("#reset").addEventListener("click", () => {
     winCountUser = 0;
     wincountComp = 0;
+    outputCompWin.innerHTML = 0;
+    outputUserWin.innerHTML = 0;
+    document.querySelector("h2").innerHTML = "The Final winner is:";
 })
 
 
 
 
 const roundsToWin = () => {
-
     const fiveChecked = document.querySelector("#five").checked;
     const tenChecked = document.querySelector("#ten").checked;
     const fifteenChecked = document.querySelector("#fifteen").checked;
@@ -141,48 +148,48 @@ const roundsToWin = () => {
     if (fiveChecked) {
 
         if (winCountUser === 5) {
-            console.log("FINAL WINNER: USER");
+            document.querySelector("h2").innerHTML = "The Final winner is: USER!!!"
             return;
         }
 
         if (wincountComp === 5) {
-            console.log("FINAL WINNER: COMP");
+            document.querySelector("h2").innerHTML = "The Final winner is: COMP!!!"
             return;
         }
     }
 
     if (tenChecked) {
         if (winCountUser === 10) {
-            console.log("FINAL WINNER: USER");
+            document.querySelector("h2").innerHTML = "The Final winner is: USER!!!"
             return;
         }
 
         if (wincountComp === 10) {
-            console.log("FINAL WINNER: COMP");
+            document.querySelector("h2").innerHTML = "The Final winner is: COMP!!!"
             return;
         }
     }
 
     if (fifteenChecked) {
         if (winCountUser === 15) {
-            console.log("FINAL WINNER: USER");
+            document.querySelector("h2").innerHTML = "The Final winner is: USER!!!"
             return;
         }
 
         if (wincountComp === 15) {
-            console.log("FINAL WINNER: COMP");
+            document.querySelector("h2").innerHTML = "The Final winner is: COMP!!!"
             return;
         }
     }
 
     if (twentyChecked) {
         if (winCountUser === 20) {
-            console.log("FINAL WINNER: USER");
+            document.querySelector("h2").innerHTML = "The Final winner is: USER!!!"
             return;
         }
 
         if (wincountComp === 20) {
-            console.log("FINAL WINNER: COMP");
+            document.querySelector("h2").innerHTML = "The Final winner is: COMP!!!"
             return;
         }
     }
