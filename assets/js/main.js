c28a009706159e525ca9de535045bf1cf14606e4
Franz-SuperCode <franzsupercode@gmail.com>
2022-11-06 19:26:02 +0100

    change function and style

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ let outputCompWin = document.querySelector("#compWin");
 //Variablen für User/COMP Auswahl (Stein, Papier oder Schere)
 let compAuswahl;
 let userAuswahl;
+let amountRounds = 0;
 
 
 steinButton.addEventListener("click", () => {
@@ -28,9 +29,10 @@ steinButton.addEventListener("click", () => {
     } else {
         console.log("Compauswahl ist Schere")
     }
-
+    amountRounds++;
     whoWin();
     roundsToWin();
+
 })
 
 papierButton.addEventListener("click", () => {
@@ -48,7 +50,7 @@ papierButton.addEventListener("click", () => {
     } else {
         console.log("Compauswahl ist Schere")
     }
-
+    amountRounds++;
     whoWin();
     roundsToWin();
 })
@@ -68,7 +70,7 @@ schereButton.addEventListener("click", () => {
     } else {
         console.log("Compauswahl ist Schere")
     }
-
+    amountRounds++;
     whoWin();
     roundsToWin();
 })
@@ -81,61 +83,58 @@ let winCountUser = 0;
 const whoWin = () => {
     if (compAuswahl === 0 && userAuswahl === 1) {
         console.log("COMP Loose");
+        document.querySelector("#textWin").innerHTML = "USER WIN"
         //Gewinn auf den Zählerstand packen
         winCountUser += 1;
 
 
     } else if (compAuswahl === 0 && userAuswahl === 2) {
         console.log("COMP Win");
+        document.querySelector("#textWin").innerHTML = "USER LOOSE"
         wincountComp += 1;
 
     } else if (compAuswahl === 0 && userAuswahl === 0) {
         console.log("Unentschieden");
-
+        document.querySelector("#textWin").innerHTML = "DRAW"
     }
 
 
     if (compAuswahl === 1 && userAuswahl === 1) {
         console.log("Unentschieden");
+        document.querySelector("#textWin").innerHTML = "DRAW"
 
 
     } else if (compAuswahl === 1 && userAuswahl === 2) {
         console.log("COMP Loose");
+        document.querySelector("#textWin").innerHTML = "USER WIN"
         winCountUser += 1;
 
 
     } else if (compAuswahl === 1 && userAuswahl === 0) {
         console.log("COMP Win");
+        document.querySelector("#textWin").innerHTML = "USER LOOSE";
         wincountComp += 1;
-
     }
 
     if (compAuswahl === 2 && userAuswahl === 1) {
         console.log("COMP Win");
+        document.querySelector("#textWin").innerHTML = "USER LOOSE"
         wincountComp += 1;
 
     } else if (compAuswahl === 2 && userAuswahl === 2) {
         console.log("Unentschieden");
+        document.querySelector("#textWin").innerHTML = "DRAW"
 
 
     } else if (compAuswahl === 2 && userAuswahl === 0) {
         console.log("COMP Loose");
+        document.querySelector("#textWin").innerHTML = "USER WIN"
         winCountUser += 1;
     }
 
     outputUserWin.innerHTML = winCountUser;
     outputCompWin.innerHTML = wincountComp;
 }
-
-// Bei reset
-document.querySelector("#reset").addEventListener("click", () => {
-    winCountUser = 0;
-    wincountComp = 0;
-    outputCompWin.innerHTML = 0;
-    outputUserWin.innerHTML = 0;
-    document.querySelector("h2").innerHTML = "The Final winner is:";
-})
-
 
 
 
@@ -146,51 +145,76 @@ const roundsToWin = () => {
     const twentyChecked = document.querySelector("#twenty").checked;
 
     if (fiveChecked) {
+        if (amountRounds === 5) {
+            if (winCountUser > wincountComp) {
+                document.querySelector("#finalWinner").innerHTML = "The Final winner is: USER!!!"
+            }
 
-        if (winCountUser === 5) {
-            document.querySelector("h2").innerHTML = "The Final winner is: USER!!!"
-            return;
-        }
+            if (wincountComp > winCountUser) {
+                document.querySelector("#finalWinner").innerHTML = "The Final winner is: COMP!!!"
 
-        if (wincountComp === 5) {
-            document.querySelector("h2").innerHTML = "The Final winner is: COMP!!!"
-            return;
+            }
+            document.querySelector("#buttons").style.display = "none";
+            document.querySelector("#finalWinner").classList.add("animationWinner");
+            document.querySelector("#finalWinner").style.fontSize = "3rem";
         }
     }
-
     if (tenChecked) {
-        if (winCountUser === 10) {
-            document.querySelector("h2").innerHTML = "The Final winner is: USER!!!"
-            return;
-        }
+        if (amountRounds === 10) {
+            if (winCountUser > wincountComp) {
+                document.querySelector("#finalWinner").innerHTML = "The Final winner is: USER!!!"
 
-        if (wincountComp === 10) {
-            document.querySelector("h2").innerHTML = "The Final winner is: COMP!!!"
-            return;
+            }
+
+            if (wincountComp > winCountUser) {
+                document.querySelector("#finalWinner").innerHTML = "The Final winner is: COMP!!!"
+
+            }
+            document.querySelector("#buttons").style.display = "none";
+            document.querySelector("#finalWinner").classList.add("animationWinner");
+            document.querySelector("#finalWinner").style.fontSize = "3rem";
         }
     }
-
     if (fifteenChecked) {
-        if (winCountUser === 15) {
-            document.querySelector("h2").innerHTML = "The Final winner is: USER!!!"
-            return;
-        }
+        if (amountRounds === 15) {
+            if (winCountUser > wincountComp) {
+                document.querySelector("#finalWinner").innerHTML = "The Final winner is: USER!!!"
+            }
 
-        if (wincountComp === 15) {
-            document.querySelector("h2").innerHTML = "The Final winner is: COMP!!!"
-            return;
+            if (wincountComp > winCountUser) {
+                document.querySelector("#finalWinner").innerHTML = "The Final winner is: COMP!!!"
+            }
+            document.querySelector("#buttons").style.display = "none";
+            document.querySelector("#finalWinner").classList.add("animationWinner");
+            document.querySelector("#finalWinner").style.fontSize = "3rem";
         }
     }
-
     if (twentyChecked) {
-        if (winCountUser === 20) {
-            document.querySelector("h2").innerHTML = "The Final winner is: USER!!!"
-            return;
-        }
+        if (amountRounds === 20) {
+            if (winCountUser > wincountComp) {
+                document.querySelector("#finalWinner").innerHTML = "The Final winner is: USER!!!"
+            }
 
-        if (wincountComp === 20) {
-            document.querySelector("h2").innerHTML = "The Final winner is: COMP!!!"
-            return;
+            if (wincountComp > winCountUser) {
+                document.querySelector("#finalWinner").innerHTML = "The Final winner is: COMP!!!"
+            }
+            document.querySelector("#buttons").style.display = "none";
+            document.querySelector("#finalWinner").classList.add("animationWinner");
+            document.querySelector("#finalWinner").style.fontSize = "3rem";
         }
     }
 }
+
+// Bei reset
+document.querySelector("#reset").addEventListener("click", () => {
+    winCountUser = 0;
+    wincountComp = 0;
+    amountRounds = 0;
+    outputCompWin.innerHTML = 0;
+    outputUserWin.innerHTML = 0;
+    document.querySelector("#finalWinner").innerHTML = "The Final winner is:";
+    document.querySelector("#textWin").innerHTML = ""
+    document.querySelector("#buttons").style.display = "flex";
+    document.querySelector("#finalWinner").classList.remove("animationWinner");
+    document.querySelector("#finalWinner").style.fontSize = "1.5rem";
+})
